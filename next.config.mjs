9c60738d642bf880e280c512415a1504c85e5a46
Siya Kadam <kadamsiya444@gmail.com>
2025-12-18 17:39:18 +0530

    Updated neubodhi-website

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,       // keep your existing setting
-  output: 'export',          // important for static export
+  //output: 'export',          // important for static export
   distDir: 'build',          // output folder matches Azure output_location
 };
 
